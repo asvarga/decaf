6b5ff22cb88f0ba561c86ad51c47c8690589db8b
thealexvarga <thealexvarga@gmail.com>
2019-09-14 23:05:16 -0400

    parsing gathers source location data

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -1,92 +1,99 @@
 #lang br/quicklang
 (require brag/support)
-(require br-parser-tools/lex-sre)
+(require (rename-in br-parser-tools/lex-sre [- --] [+ ++]))
 
 ;; see http://cs.brown.edu/courses/cs126/decaf-syntax.pdf
 
 (define (make-tokenizer port)
+  (port-count-lines! port) ; <- turn on line & column counting
   (define (next-token)
     (define decaf-lexer
       (lexer
 
        [nothing
-        (token 'TEMPLATE-TOK lexeme)]
+        (token+ 'TEMPLATE-TOK "" lexeme "" lexeme-start lexeme-end)]
+
+       ;; DECAF++
+       [(: "<" (* upper-case) ">")
+        (token+ 'LANG-START-TOK "<" lexeme ">" lexeme-start lexeme-end)]
+       [(: "</" (* upper-case) ">")
+        (token+ 'LANG-END-TOK "</" lexeme ">" lexeme-start lexeme-end)]
 
        ;; keywords
        [(or "break" "class" "continue" "else" "extends"
             "if" "new" "private" "protected" "public"
             "return" "static" "super" "this" "while")
-        (token 'KEYWORD-TOK lexeme)]
+        (token+ 'KEYWORD-TOK "" lexeme "" lexeme-start lexeme-end)]
        [(or "abstract" "byte" "case" "catch" "const"
             "default" "do" "double" "final" "finally"
             "for" "implements" "import" "instanceof" "interface"
             "long" "native" "goto" "package" "short"
             "switch" "synchronized" "throw" "throws" "transient"
             "try" "volatile")
-        (token 'FORBIDDEN-TOK lexeme)]
+        (token+ 'FORBIDDEN-TOK "" lexeme "" lexeme-start lexeme-end)]
        [(or "byvalue" "cast" "future" "generic" "inner"
             "none" "operator" "outer" "rest" "var")
-        (token 'RESERVED-TOK lexeme)]
+        (token+ 'RESERVED-TOK "" lexeme "" lexeme-start lexeme-end)]
 
        ;; types
        [(or "boolean" "char" "int" "void")
-        (token 'PRIM-TYPE-TOK lexeme)]
+        (token+ 'PRIM-TYPE-TOK "" lexeme "" lexeme-start lexeme-end)]
        [(or "byte" "double" "float" "long" "short")
-        (token 'FORBIDDEN-TYPE-TOK lexeme)]
+        (token+ 'FORBIDDEN-TYPE-TOK "" lexeme "" lexeme-start lexeme-end)]
 
        ;; identifiers
        [(: (or "_" alphabetic) (* (or "_" alphabetic numeric)))
-        (token 'IDENTIFIER-TOK lexeme)]
+        (token+ 'IDENTIFIER-TOK "" lexeme "" lexeme-start lexeme-end)]
 
        ;; comments
        [(or (from/to "//" "\n") (from/to "/*" "*/"))
-        (token 'COMMENT-TOK lexeme #:skip? #t)]
+        ;;(token 'COMMENT-TOK lexeme #:skip? #t)]
+        (next-token)]
 
        ;; literals
-       [(or "0" (: (- numeric "0") (* numeric)))
-        (token 'INT-LIT-TOK lexeme)]
+       [(or "0" (: (-- numeric "0") (* numeric)))
+        (token+ 'INT-LIT-TOK "" lexeme "" lexeme-start lexeme-end)]
        [nothing  ;; unimplemented
-        (token 'FLOAT-LIT-TOK lexeme)]
-       [(: "'" (- any-char "\\" "\n" "'") "'")
-        (token 'CHAR-LIT-TOK (trim-ends "'" lexeme "'"))]
-       [(: "'\\" (- any-char "n" "t") "'")
-        (token 'CHAR-LIT-TOK (trim-ends "'\\" lexeme "'"))]
+        (token+ 'FLOAT-LIT-TOK "" lexeme "" lexeme-start lexeme-end)]
+       [(: "'" (-- any-char "\\" "\n" "'") "'")
+        (token+ 'CHAR-LIT-TOK "'" lexeme "'" lexeme-start lexeme-end)]
+       [(: "'\\" (-- any-char "n" "t") "'")
+        (token+ 'CHAR-LIT-TOK "'\\" lexeme "'" lexeme-start lexeme-end)]
        ["'\\t'"
         (token 'CHAR-LIT-TOK "\t")]
        ["'\\n'"
         (token 'CHAR-LIT-TOK "\n")]
        [(: "\""
-           (* (or (- any-char "\\" "\n" "\"")  ;; don't match single \
+           (* (or (-- any-char "\\" "\n" "\"")  ;; don't match single \
                   (: "\\" any-char)))          ;; \ captures next char
            "\"")
         (token 'STRING-LIT-TOK                 ;; clean using str-lexer
-               (clean (trim-ends "\"" lexeme "\"")))]
+                (clean (trim-ends "\"" lexeme "\""))
+                #:position (+ (pos lexeme-start) 1)
+                #:line (line lexeme-start)
+                #:column (+ (col lexeme-start) 1)
+                #:span (- (pos lexeme-end)
+                          (pos lexeme-start) 2))]
        [(or "true" "false")
-        (token 'BOOLEAN-LIT-TOK lexeme)]
+        (token+ 'BOOLEAN-LIT-TOK "" lexeme "" lexeme-start lexeme-end)]
        ["null"
-        (token 'NULL-LIT-TOK lexeme)]
+        (token+ 'NULL-LIT-TOK "" lexeme "" lexeme-start lexeme-end)]
 
        ;; punctuation
        [(or "(" ")" "{" "}" "[" "]" ";" "," ".")
-        (token 'PUNCTUATION-TOK lexeme)]
-
-       ;; DECAF++
-       [(: "<" (* upper-case) ">")
-        (token 'LANG-START-TOK (trim-ends "<" lexeme ">"))]
-       [(: "</" (* upper-case) ">")
-        (token 'LANG-END-TOK (trim-ends "</" lexeme ">"))]
+        (token+ 'PUNCTUATION-TOK "" lexeme "" lexeme-start lexeme-end)]
 
        ;; operators
        [(or "=" ">" "<" "!"
             "==" ">=" "<=" "!="
             "+" "-" "*" "/"
             "&&" "||" "%")
-        (token 'OPERATOR-TOK lexeme)]
+        (token+ 'OPERATOR-TOK "" lexeme "" lexeme-start lexeme-end)]
        [(or "~" "?" ":" "++" "--"
             "&" "|" "^" "<<" ">>" ">>>"
             "+=" "-=" "*=" "/=" "&=" "|="
             "^=" "%=" "<<=" ">>=" ">>>=")
-        (token 'FORBIDDEN-OPERATOR-TOK lexeme)]
+        (token+ 'FORBIDDEN-OPERATOR-TOK "" lexeme "" lexeme-start lexeme-end)]
 
        ;; otherwise
        [whitespace (next-token)]
@@ -100,8 +107,8 @@
 
 (define str-lexer
   (lexer
-   [(- any-char "\\" "\n" "\"") lexeme]
-   [(: "\\" (- any-char "n" "t")) (trim-ends "\\" lexeme "")]
+   [(-- any-char "\\" "\n" "\"") lexeme]
+   [(: "\\" (-- any-char "n" "t")) (trim-ends "\\" lexeme "")]
    ["\\t" "\t"]
    ["\\n" "\n"]
    [any-char (error 'absurd)]  ;; unreachable when passed proper string contents
@@ -111,7 +118,20 @@
   (string-join (apply-lexer str-lexer str) "")
   )
 
+;(define-macro (token+ type val)
+;  (token type val)
+;  )
 
+(define (token+ type left lex right lex-start lex-end)
+  (let ([l0 (string-length left)] [l1 (string-length right)])
+    (token type (trim-ends left lex right)
+           #:position (+ (pos lex-start) l0)
+           #:line (line lex-start)
+           #:column (+ (col lex-start) l0)
+           #:span (- (pos lex-end)
+                     (pos lex-start) l0 l1))
+    )
+  )
 
 
 
